07af6252e5d15e6ccb1630b82d9fe48f1c1565c6
satiati <162111295+satiati@users.noreply.github.com>
2024-04-22 09:44:57 +0200

    added Technology.java

---
+++ Technology.java
@@ -0,0 +1,13 @@
+public class Technology {
+    private String name;
+    private int bonus;
+
+    public Technology(String name, int bonus) {
+        this.name = name;
+        this.bonus = bonus;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+}
